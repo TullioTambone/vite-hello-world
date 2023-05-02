d1cf8c334b5c77a4ee670a5b92b951bae8719d98
TullioTambone <tulliotambone97@gmail.com>
2023-05-02 14:24:42 +0200

    Creazionee titolo

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,25 @@
+<script>
+    export default{
+        name: "myTitle",
+        data() {
+            return {
+                name: 'helloWorld',
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+    </div>
+</template>
+
+<style scoped>
+
+    h1{
+        color: red;
+    }
+
+</style>
